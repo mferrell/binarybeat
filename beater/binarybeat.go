454e1c3e806d6798d68004bbd0f91568ac78b861
Matthew Ferrell <Matthew.Ferrell@walmart.com>
2016-10-24 16:42:09 -0400

    Copied filebeat base.

--- beater/binarybeat.go
+++ beater/binarybeat.go
@@ -1,62 +1,173 @@
 package beater
 
 import (
+	"flag"
 	"fmt"
-	"time"
+	"sync"
 
 	"github.com/elastic/beats/libbeat/beat"
 	"github.com/elastic/beats/libbeat/common"
 	"github.com/elastic/beats/libbeat/logp"
-	"github.com/elastic/beats/libbeat/publisher"
+	
+	cfg "github.com/mferrell/binarybeat/config"
+	"github.com/mferrell/binarybeat/crawler"
+	"github.com/mferrell/binarybeat/publisher"
+	"github.com/mferrell/binarybeat/registrar"
+	"github.com/mferrell/binarybeat/spooler"
 
-	"github.com/mferrell/binarybeat/config"
 )
 
+var once = flag.Bool("once", false, "Run filebeat only once until all harvesters reach EOF")
+
+// Binarybeat is a beater object. Contains all objects needed to run the beat
 type Binarybeat struct {
+	config *cfg.Config
 	done   chan struct{}
-	config config.Config
-	client publisher.Client
 }
 
-// Creates beater
-func New(b *beat.Beat, cfg *common.Config) (beat.Beater, error) {
-	config := config.DefaultConfig
-	if err := cfg.Unpack(&config); err != nil {
+// New creates a new Binarybeat pointer instance.
+func New(b *beat.Beat, rawConfig *common.Config) (beat.Beater, error) {
+	config := cfg.DefaultConfig
+	if err := rawConfig.Unpack(&config); err != nil {
 		return nil, fmt.Errorf("Error reading config file: %v", err)
 	}
-
-	bt := &Binarybeat{
-		done: make(chan struct{}),
-		config: config,
+	if err := config.FetchConfigs(); err != nil {
+		return nil, err
 	}
-	return bt, nil
+
+	fb := &Binarybeat{
+		done:   make(chan struct{}),
+		config: &config,
+	}
+	return fb, nil
 }
 
-func (bt *Binarybeat) Run(b *beat.Beat) error {
-	logp.Info("binarybeat is running! Hit CTRL-C to stop it.")
+// Run allows the beater to be run as a beat.
+func (fb *Binarybeat) Run(b *beat.Beat) error {
+	var err error
+	config := fb.config
 
-	bt.client = b.Publisher.Connect()
-	ticker := time.NewTicker(bt.config.Period)
-	counter := 1
-	for {
-		select {
-		case <-bt.done:
-			return nil
-		case <-ticker.C:
-		}
+	waitFinished := newSignalWait()
+	waitEvents := newSignalWait()
 
-		event := common.MapStr{
-			"@timestamp": common.Time(time.Now()),
-			"type":       b.Name,
-			"counter":    counter,
-		}
-		bt.client.PublishEvent(event)
-		logp.Info("Event sent")
-		counter++
+	// count active events for waiting on shutdown
+	wgEvents := &sync.WaitGroup{}
+	finishedLogger := newFinishedLogger(wgEvents)
+
+	// Setup registrar to persist state
+	registrar, err := registrar.New(config.RegistryFile, finishedLogger)
+	if err != nil {
+		logp.Err("Could not init registrar: %v", err)
+		return err
 	}
+
+	// Make sure all events that were published in
+	registrarChannel := newRegistrarLogger(registrar)
+
+	// Channel from spooler to harvester
+	publisherChan := newPublisherChannel()
+
+	// Publishes event to output
+	publisher := publisher.New(config.PublishAsync, publisherChan.ch, registrarChannel, b.Publisher)
+
+	// Init and Start spooler: Harvesters dump events into the spooler.
+	spooler, err := spooler.New(config, publisherChan)
+	if err != nil {
+		logp.Err("Could not init spooler: %v", err)
+		return err
+	}
+
+	crawler, err := crawler.New(newSpoolerOutlet(fb.done, spooler, wgEvents), config.Prospectors)
+	if err != nil {
+		logp.Err("Could not init crawler: %v", err)
+		return err
+	}
+
+	// The order of starting and stopping is important. Stopping is inverted to the starting order.
+	// The current order is: registrar, publisher, spooler, crawler
+	// That means, crawler is stopped first.
+
+	// Start the registrar
+	err = registrar.Start()
+	if err != nil {
+		return fmt.Errorf("Could not start registrar: %v", err)
+	}
+	// Stopping registrar will write last state
+	defer registrar.Stop()
+
+	// Start publisher
+	publisher.Start()
+	// Stopping publisher (might potentially drop items)
+	defer func() {
+		// Closes first the registrar logger to make sure not more events arrive at the registrar
+		// registrarChannel must be closed first to potentially unblock (pretty unlikely) the publisher
+		registrarChannel.Close()
+		publisher.Stop()
+	}()
+
+	// Starting spooler
+	spooler.Start()
+
+	// Stopping spooler will flush items
+	defer func() {
+		// Wait for all events to be processed or timeout
+		waitEvents.Wait()
+
+		// Closes publisher so no further events can be sent
+		publisherChan.Close()
+		// Stopping spooler
+		spooler.Stop()
+	}()
+
+	err = crawler.Start(registrar.GetStates(), *once)
+	if err != nil {
+		return err
+	}
+
+	// If run once, add crawler completion check as alternative to done signal
+	if *once {
+		runOnce := func() {
+			logp.Info("Running binarybeat once. Waiting for completion ...")
+			crawler.WaitForCompletion()
+			logp.Info("All data collection completed. Shutting down.")
+		}
+		waitFinished.Add(runOnce)
+	}
+
+	// Add done channel to wait for shutdown signal
+	waitFinished.AddChan(fb.done)
+	waitFinished.Wait()
+
+	// Stop crawler -> stop prospectors -> stop harvesters
+	// Note: waiting for crawlers to stop here in order to install wgEvents.Wait
+	//       after all events have been enqueued for publishing. Otherwise wgEvents.Wait
+	//       or publisher might panic due to concurrent updates.
+	crawler.Stop()
+
+	timeout := fb.config.ShutdownTimeout
+	// Checks if on shutdown it should wait for all events to be published
+	waitPublished := fb.config.ShutdownTimeout > 0 || *once
+	if waitPublished {
+		// Wait for registrar to finish writing registry
+		waitEvents.Add(withLog(wgEvents.Wait,
+			"Continue shutdown: All enqueued events being published."))
+		// Wait for either timeout or all events having been ACKed by outputs.
+		if fb.config.ShutdownTimeout > 0 {
+			logp.Info("Shutdown output timer started. Waiting for max %v.", timeout)
+			waitEvents.Add(withLog(waitDuration(timeout),
+				"Continue shutdown: Time out waiting for events being published."))
+		} else {
+			waitEvents.AddChan(fb.done)
+		}
+	}
+
+	return nil
 }
 
-func (bt *Binarybeat) Stop() {
-	bt.client.Close()
-	close(bt.done)
+// Stop is called on exit to stop the crawling, spooling and registration processes.
+func (fb *Binarybeat) Stop() {
+	logp.Info("Stopping binarybeat")
+
+	// Stop Binarybeat
+	close(fb.done)
 }
